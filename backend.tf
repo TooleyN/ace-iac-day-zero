3b667b530dbfaf8582ccca682184baeb0a65ba76
TooleyN <66574168+TooleyN@users.noreply.github.com>
2022-01-19 16:43:38 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "NT-Cloud"
+    organization = "NT-Cloud"
     workspaces {
       name = "ace-iac-day-zero"
     }
